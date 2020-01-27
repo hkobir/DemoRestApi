1018ae8fb401565d9b7f630f4356090f5e54a59f
hkobir <hkobir10@gmail.com>
2020-01-27 22:56:27 +0600

    add signUp layout

--- Android/app/src/main/java/com/hk/demoapiuser/MainActivity.java
+++ Android/app/src/main/java/com/hk/demoapiuser/MainActivity.java
@@ -1,14 +1,25 @@
 package com.hk.demoapiuser;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.hk.demoapiuser.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(binding.emailEt.getText().toString().equals(""))
+            }
+        });
     }
 }
